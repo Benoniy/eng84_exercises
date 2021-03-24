5d6c9955d146d9830cc60a3b4bb3fb793fddc9a6
Ben <benranson5@gmail.com>
2021-03-24 16:31:52 +0000

    added comments and additional functionality

--- Task_2_Restaurant_Waiter_Helper/resturant_waiter_helper.py
+++ Task_2_Restaurant_Waiter_Helper/resturant_waiter_helper.py
@@ -1,34 +1,46 @@
 # Create a program that helps a waiter with his menu and his orders.
+
+# A dictionary that contains all the items a customer can order
 menu = {1: {"name": "Burger", "price": 3.0},
         2: {"name": "Fries", "price": 1.2},
         3: {"name": "Chicken wings", "price": 4.0}
         }
 
+# A list that stored the customers ordered items
 order = []
 
 
+# This function prints the menu by extracting values from a dictionary
 def print_menu():
     print("Menu:")
     for item_no, content in menu.items():
         print(f"{item_no}: {content['name']} £{content['price']}")
 
 
+# This function prints orders by pulling the item numbers from the list
+# and using them to reference values in the menu dictionary
 def print_order():
     total = 0
     print("\nCurrent Order:")
     for item in order:
         print(f"{menu[item]['name']} £{menu[item]['price']}")
         total += menu[item]['price']
-    print(f"Total - £{total}")
+    print(f"Total = £{total}\n")
 
 
 print_menu()
+
+# This controls user input
 print("\n")
 usr_input = ""
 while usr_input != "exit":
-    usr_input = input("Please enter the number of the item you wish to add:\n")
+    usr_input = input("Commands: exit | clear \nPlease enter the number of the item you wish to add:\n")
 
-    if usr_input.isdigit():
+    if usr_input.isdigit():  # If its a digit treat it as an order
         order.append(int(usr_input))
+    else:  # else treat it as a command
+        usr_input = usr_input.lower()
+        if usr_input == "clear":
+            order.clear()
 
     print_order()
