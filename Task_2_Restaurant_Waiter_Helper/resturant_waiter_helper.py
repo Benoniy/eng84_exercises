15dae0ca4beda081751a253cf954d28d2e0ed5fd
Ben <benranson5@gmail.com>
2021-03-24 16:22:10 +0000

    generated a dictionary and a list to use for the system

---
+++ Task_2_Restaurant_Waiter_Helper/resturant_waiter_helper.py
@@ -0,0 +1,10 @@
+# Create a program that helps a waiter with his menu and his orders.
+menu = {1: {"name": "Burger", "price": 3.0},
+        2: {"name": "Fries", "price": 1.2},
+        3: {"name": "Chicken wings", "price": 4.0}
+        }
+
+order = []
+
+
+
