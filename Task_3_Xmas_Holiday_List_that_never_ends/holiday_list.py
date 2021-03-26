88214868fbc131b4acf953334bb6e212b43ec717
Ben <benranson5@gmail.com>
2021-03-26 11:09:40 +0000

    compiled code into functions to make it DRY

--- Task_3_Xmas_Holiday_List_that_never_ends/holiday_list.py
+++ Task_3_Xmas_Holiday_List_that_never_ends/holiday_list.py
@@ -1,19 +1,30 @@
 
 
-xmas_list = []
-print("HO HO HO! What do you want for Christmas?")
+def generate_list():
+    xmas_list = []
+    print("\nHO HO HO! What do you want for Christmas?")
 
-user_input = ""
-while True:
-    user_input = input()
+    user_input = ""
+    while True:
+        user_input = input()
 
-    if user_input.lower().count("exit") > 0:
-        break
+        if user_input.lower().count("exit") > 0:
+            break
 
-    xmas_list.append(user_input)
-    print("What else would you like?")
+        xmas_list.append(user_input)
+        print("What else would you like?")
 
-count = 1
-for item in xmas_list:
-    print(f"{count}. {item.upper()}")
-    count += 1
+    return xmas_list
+
+
+def print_list(item_list):
+    print("\nMy Christmas List:")
+    count = 1
+    for item in item_list:
+        print(f"{count}. {item.upper()}")
+        count += 1
+
+
+if __name__ == "__main__":
+    xmas_list = generate_list()
+    print_list(xmas_list)
